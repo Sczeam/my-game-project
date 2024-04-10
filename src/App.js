8a8db41caa1a43eef59d2dd6efeaf8f2da8126c8
Sczeam <zawhtikeaung19@gmail.com>
2024-04-10 02:06:57 +0100

    gameflow

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-// import MainMenu from "./components/MainMenu";
+// App.js
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import MainMenu from "./components/MainMenu";
+import Game from "./components/Game";
+import Settings from "./components/Settings";
 
-// function App() {
-//   return (
-//     <div>
-//       <MainMenu />
-//       <MainMenu />
-//     </div>
-//   );
-// }
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainMenu />} />
+        <Route path="/game" element={<Game />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
+    </Router>
+  );
+}
 
-// export default App;
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,17 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import MainMenu from "./components/MainMenu";
-import Game from "./components/Game";
-import Settings from "./components/Settings";
-
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainMenu />}>
-          <Route path="MainMenu" element={<MainMenu />} />
-          <Route path="Game" element={<Game />} />
-          <Route path="Settings" element={<Settings />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
